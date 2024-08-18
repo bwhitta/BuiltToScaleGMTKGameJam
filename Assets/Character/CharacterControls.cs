54de1c516abf420617842f75b9e360f9281802d2
bwhitta <whittakerb1987@gmail.com>
2024-08-17 22:55:57 -0400

    Revert "camera moves with window"
    This reverts commit a8437d21fe6df6a70ce383c3bf33efd39fcbe812.

--- Assets/Character/CharacterControls.cs
+++ Assets/Character/CharacterControls.cs
@@ -1,39 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterControls : MonoBehaviour
 {
-    [SerializeField] private float jumpVelocity;
-    public float baseSpeed;
-    [HideInInspector] public float speed;
-
+    [SerializeField] private float speed, jumpVelocity;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-
-    private Rigidbody2D playerRigidbody;
+    private Rigidbody2D _playerRigidbody;
+    private Rigidbody2D PlayerRigidbody
+    {
+        get
+        {
+            if (_playerRigidbody == null)
+            {
+                _playerRigidbody = GetComponent<Rigidbody2D>();
+            }
+            return _playerRigidbody;
+        }
+    }
 
     private bool Grounded => Physics2D.OverlapCircle(groundCheck.position, 0.01f, groundLayer) != null;
-    private void Start()
-    {
-        playerRigidbody = GetComponent<Rigidbody2D>();
-    }
+
     void FixedUpdate()
     {
-        // Reset velo  city
-        playerRigidbody.velocity *= new Vector2(0, 1);
+        // Reset velocity
+        PlayerRigidbody.velocity *= new Vector2(0, 1);
         
         // Check inputs
         if (Input.GetKey(KeyCode.A))
         {
-            playerRigidbody.velocity += Vector2.left * speed;
+            PlayerRigidbody.velocity += Vector2.left * speed;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-             playerRigidbody.velocity += Vector2.right * speed;
+             PlayerRigidbody.velocity += Vector2.right * speed;
         }
         if (Input.GetKey(KeyCode.W) && Grounded)
         {
-            playerRigidbody.velocity *= new Vector2(1, 0);
-            playerRigidbody.velocity += jumpVelocity * Screen.currentResolution.height * Vector2.up/Screen.height;
+            PlayerRigidbody.velocity *= new Vector2(1, 0);
+            PlayerRigidbody.velocity += jumpVelocity * Screen.currentResolution.height * Vector2.up/Screen.height;
         }
     }
 }
--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,38 +6,21 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] private float followTime = 0.3F;
 
     [SerializeField] private float scaledPositionY;
-    [SerializeField] private float zPosition;
+    [SerializeField] private float relativePositionZ;
     private Vector2 velocity = Vector2.zero;
-
-    private void Start()
-    {
-        transform.position = character.position;
-    }
 
     void Update()
     {
-        Vector2 monitorCenter = new Vector2(ResolutionManager.width, ResolutionManager.height) / 2;
-        Vector2 windowCenter = Screen.mainWindowPosition + (new Vector2(Screen.width, Screen.height) / 2);
-        Vector2 windowOffset = (monitorCenter - windowCenter);
-        windowOffset *= new Vector2(-1, 1);
-        Vector2 offsetPercent = (windowOffset / ResolutionManager.Size);
-
-        // finds the 1:X aspect ratio
-        float aspectRatio = ResolutionManager.width / ResolutionManager.height;
-        float cameraFullWidth = Camera.main.orthographicSize * 2 * aspectRatio;
-        float cameraFullHeight = Camera.main.orthographicSize * 2;
+        //float positionY = scaledPositionY * (Screen.height / Screen.currentResolution.height); <- temporarily removed
+        Vector3 relativePosition = new(0, 0/*positionY*/, relativePositionZ);
 
         // Choose the target position
-        float cameraTargetX = cameraFullWidth * offsetPercent.x * transform.lossyScale.x;
-        float cameraTargetY =  cameraFullHeight * offsetPercent.y * transform.lossyScale.y;
-
-        Vector3 targetPosition = character.position + new Vector3(cameraTargetX, cameraTargetY, 0);
-        Debug.Log($"camera orthographicSize: {Camera.main.orthographicSize}, camera is orthographic: {Camera.main.orthographic}, offsetPercent: {offsetPercent}, targetPosition: {targetPosition}, cameraTargetX: {cameraTargetX}, cameraTargetY: {cameraTargetY}");
-
+        Vector3 targetPosition = character.TransformPoint(relativePosition);
+        
         // Move the camera towards that position
         Vector2 position2d = Vector2.SmoothDamp(transform.position, targetPosition, ref velocity, followTime);
         
         // The z position is locked, since it shouldn't really ever change in this game.
-        transform.position = new Vector3(position2d.x, position2d.y, zPosition);
+        transform.position = new Vector3(position2d.x, position2d.y, relativePosition.z);
     }
 }
